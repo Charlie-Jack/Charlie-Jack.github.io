5f9aa233f47d2b188171fa88a33d3c64372b0d77
Charlie-Jack <charliejacksimpkins@gmail.com>
2025-09-01 20:37:07 +0100

    Update pathfind.js
    Edit to new version - end of first iteration of development.

--- scripts/pathfind.js
+++ scripts/pathfind.js
@@ -1,6 +1,5 @@
-//A* pathfinding algorithm.
-//CharlieJack, from The Coding Train.
-//15.04.25 
+/*Created by: Charlie Simpkins
+Last updated: 30-08-2025 */
 
 //Utility functions.
 function removeFromArray(array, element) {
@@ -22,6 +21,33 @@ function heuristic(a, b) { //Finds distance between points.
 //Import p5 for on-demand global mode (ignore error message).
 new p5();
 
+/*Created by: Charlie Simpkins
+Last updated: 30-08-2025
+Resets the pathfinding algorithm when the player moves, as the conditions have now changed.
+This includes both the open and closed set, previously visited nodes, and the pathfinding boolean.
+Also calls enemyPathfinding() every seven frames to cause the enemy towards the player, even when the
+player is also moving.*/
+
+let enemySpeed = 0;
+function resetPathfind() {
+    pathfind = true;
+    closedSet = [];
+    openSet = [];
+    openSet.push(path[0]);
+    for (var i = 0; i < columns; i++) { //Resets all previous nodes in the path.
+        for (var j = 0; j < rows; j++) {
+            grid[i][j].previous = undefined;
+        }
+    }
+    //Runs the enemy pathfinding and movement to cause the enemy to dash towards the player.
+    enemyMovement = 29;
+    if (enemySpeed % 7 == 0) {
+        enemyPathfinding();
+    }
+    enemySpeed++;
+}
+
+
 //Classes.
 //Player class.
 class Player {
@@ -29,7 +55,7 @@ class Player {
         this.x = x;
         this.y = y;
         this.z = z;
-        this.speed = 25;
+        this.speed = 5;
         this.position = createVector(this.x, this.y, this.z);
     }
     //Updates position based on movement key pressed.
@@ -37,23 +63,28 @@ class Player {
       let mvmt = createVector(0, 0, 0);
       //Support for player keyboard inputs.
       if(keyIsDown(65) === true) {
-        if (this.x >= -240) {
+        if (this.x >= -300) {
+            
             mvmt.x -= 1;
+            resetPathfind();
         }
       }
       if(keyIsDown(68) === true) {
-        if (this.x <= 300) {
+        if (this.x <= 275) {
             mvmt.x += 1;
+            resetPathfind();
         }
       }
       if(keyIsDown(87) === true) {
-        if (this.z >= -270) {
+        if (this.z >= -300) {
             mvmt.z -= 1;
+            resetPathfind();
         }
       }
       if(keyIsDown(83) === true) {
-        if (this.z <= 270) {
+        if (this.z <= 275) {
             mvmt.z += 1;
+            resetPathfind();        
         }
       }
 
@@ -67,6 +98,15 @@ class Player {
       this.position.set(this.x, 0, this.z);
       console.log("Player: " + this.position.toString());
 
+      //Find the current cell that the player is most closely contained within.
+      for (var a = 0; a < columns; a++) { 
+            for (var b = 0; b < rows; b++) {
+                if (grid[a][b].i == round((this.x + 300) / 20) && grid[a][b].j == round((this.z + 300) / 20)) {
+                    grid[a][b].show(color(255, 255, 0));
+                    endNode = grid[a][b];
+                }
+            }
+       }
       //Compute normal vectors.
       //console.log(normal(this.position).toString());
     }
@@ -98,7 +138,7 @@ class Enemy {
     updatePosition() {  
         this.x = path[0].i * 20 - 300;
         this.z = path[0].j * 20 - 300;
-        
+ 
         //this.position.set(this.x, this.y, this.z);
         this.position.add(p5.Vector.sub(this.position, pathPosition).limit(10));
         console.log("Enemy: " + this.position);
@@ -160,16 +200,22 @@ function Cell(i, j) {
 
     //Generate walls.
     this.wall = false;
-    if (random(1) < 0.3) {
+    if (random(1) < 0.1) {
         this.wall = true;
     }
     //Display cell in grid.
     this.show = function(colour) { 
         fill(colour);
         if (this.wall) {
-            fill(0);
+            fill(100, 100);
+
+            push();
+            translate(this.i * 20 - 300, -5, this.j * 20 - 300);
+            fill(100, 100, 100);
+            box(20, 20, 20);
+            pop(); 
         }
-        stroke(0);
+        stroke(0);      
         push(); //Convert to 3D horizontal plane.
         translate(this.i * 20 - 300, 0, this.j * 20 - 300);
         rotate(HALF_PI, [1, 0, 0]);
@@ -208,50 +254,8 @@ function Cell(i, j) {
     }
 }
 
-function setup() {
-    createCanvas(windowWidth, windowHeight, WEBGL);
-    //Instantiate camera.
-    mainCamera = createCamera();
 
-    //Make grid.
-    for (var i = 0; i < columns; i++) { //Create rows
-        grid[i] = new Array(rows);
-    }
-    for (var i = 0; i < columns; i++) { //Add cells to rows.
-        for (var j = 0; j < rows; j++) {
-            grid[i][j] = new Cell(i, j);
-        }
-    }
-    for (var i = 0; i < columns; i++) {  //Determines adjacent nodes.
-        for (var j = 0; j < rows; j++) {
-            grid[i][j].addAdjacent(grid);
-        }
-    }
-
-    //Initilaise open set and start/end nodes.
-    startNode = grid[0][0]; 
-    endNode = grid[columns - 1][rows - 1]; 
-    //Prevents start and end nodes from being blocked.
-    startNode.wall = false;
-    endNode.wall = false;
-
-    openSet.push(startNode);
-
-    //enemyPosition = createVector(enemy.getPosition('x'), enemy.getPosition('y'), enemy.getPosition('z'));
-    pathPosition = createVector(100, 0, 100);
-}
-
-function draw() {
-    //Styling.
-    ortho();
-    strokeWeight(2);
-    background(200);
-
-    //Camera control.
-    mainCamera.lookAt(0, 0, 0);
-    mainCamera.setPosition(290, -290, 290);
-    orbitControl(1, 0, 1); //Limited camera control with the mouse on the x and z axis.
-
+function enemyPathfinding() {
     if (openSet.length > 0) { //Continue if openSet is not empty - and thus the end node has not been located, nor all possible nodes exhausted.
         //Finds the most optimal node.
         var lowestIndex = 0;
@@ -266,7 +270,7 @@ function draw() {
         //Checks whether current node is the end node.
         if (currentNode === endNode) {
             console.log("Completed.");
-            noLoop();
+            pathfind = false;
         }
 
         //Moves current node from the open set to the closed set.
@@ -301,10 +305,85 @@ function draw() {
         }
     } else { //Stops search if no solution is available.
         console.log('No solution.');
-        noLoop();
+        pathfind = false;
         return;
     }
 
+    //Displays optimal path.
+    path = [];
+    var temp = currentNode;
+    path.push(temp);
+    while (temp.previous) {
+        path.push(temp.previous);
+        temp = temp.previous;
+    }    
+    console.log("Path: " + pathPosition.toString()); 
+    console.log(path);
+    
+    if (!pathfind) {
+        closedSet = [];
+        openSet = [];
+    }
+}
+
+function setup() {  
+    createCanvas(windowWidth, windowHeight, WEBGL);
+    //Instantiate camera.
+    mainCamera = createCamera();
+
+    //Make grid.
+    for (var i = 0; i < columns; i++) { //Create rows
+        grid[i] = new Array(rows);
+    }
+    for (var i = 0; i < columns; i++) { //Add cells to rows.
+        for (var j = 0; j < rows; j++) {
+            grid[i][j] = new Cell(i, j);
+        }
+    }
+    for (var i = 0; i < columns; i++) {  //Determines adjacent nodes.
+        for (var j = 0; j < rows; j++) {
+            grid[i][j].addAdjacent(grid);
+        }
+    }
+
+    //Initilaise open set and start/end nodes.
+    startNode = grid[0][0]; 
+    endNode = grid[columns - 1][rows - 1]; 
+    //Prevents start and end nodes from being blocked.
+    startNode.wall = false;
+    endNode.wall = false;
+
+    openSet.push(startNode);
+
+    //enemyPosition = createVector(enemy.getPosition('x'), enemy.getPosition('y'), enemy.getPosition('z'));
+    pathPosition = createVector(100, 0, 100);
+}
+
+let enemyMovement = 29;
+let pathfind = true;
+function draw() {
+    //Styling.
+    ortho();
+    strokeWeight(2);
+    background(200);
+
+    //Camera control.
+    mainCamera.lookAt(0, 0, 0);
+    mainCamera.setPosition(290, -290, 290);
+    orbitControl(1, 0, 1); //Limited camera control with the mouse on the x and z axis.
+
+    enemyMovement++;
+    //Enemy display.    
+    enemy.checkCollisions();
+
+    if (pathfind) {
+        if (enemyMovement % 30 == 0) {
+            enemyPathfinding();
+            enemy.updatePosition();
+        }
+        enemy.draw();
+    }
+    
     //Display cells in grid.
     for (var i = 0; i < columns; i++) { //Add cells to rows.
         for (var j = 0; j < rows; j++) {
@@ -318,26 +397,11 @@ function draw() {
     for (var i = 0; i < openSet.length; i++) { //Colours nodes of open set.
         openSet[i].show(color(0, 255, 0));
     }
-
-    //Displays optimal path.
-    path = [];
-    var temp = currentNode;
-    path.push(temp);
-    while (temp.previous) {
-        path.push(temp.previous);
-        temp = temp.previous;
-    }    
     for (var i = 0; i < path.length; i++) { //Colours nodes of path set.
         path[i].show(color(0, 0, 255));
         //Updates final path node position vector.
         pathPosition.set(path[0].i * 20 - 300, 0, path[0].j * 20 - 300);
     }
-    console.log("Path: " + pathPosition.toString());
-
-    //Enemy display.    
-    enemy.checkCollisions();
-    enemy.updatePosition();
-    enemy.draw();
 
     //Player display.
     player.updatePosition();
